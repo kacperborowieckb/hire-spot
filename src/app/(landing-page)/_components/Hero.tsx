9024538e4e2f37d3e4d3719ae9572223bdfbb99d
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-11-09 19:36:56 +0100

    add framer motion + animate the hero section

--- src/app/(landing-page)/_components/Hero.tsx
+++ src/app/(landing-page)/_components/Hero.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Button from "~/app/ui/Button";
+import { motion } from "framer-motion";
+import { fadeInAnimationVariants } from "~/app/utils/variants";
 
 export default function Hero() {
   return (
     <div className="m-4 mt-16 flex flex-col items-center justify-center gap-8 sm:m-8 sm:mt-24 md:mt-32 lg:flex-row lg:gap-16">
-      <div className="flex max-w-full flex-col text-center lg:max-w-md lg:text-left">
-        <h1 className="mb-4 text-4xl font-bold sm:text-5xl">
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{ staggerChildren: 0.15 }}
+        className="flex max-w-full flex-col text-center lg:max-w-md lg:text-left"
+      >
+        <motion.h1
+          className="mb-4 text-4xl font-bold sm:text-5xl"
+          variants={fadeInAnimationVariants}
+        >
           Revolutionize Your{" "}
           <span className="text-main-600">Hiring Process</span>
           <span className="relative mb-4 flex flex-col">
@@ -20,21 +32,28 @@ export default function Hero() {
               className="absolute left-1/2 top-full -translate-x-1/2 transform lg:left-0 lg:translate-x-0"
             />
           </span>
-        </h1>
-        <p className="mb-4 text-lg">
+        </motion.h1>
+        <motion.p className="mb-4 text-lg" variants={fadeInAnimationVariants}>
           Discover a Smarter Way to Recruit, Streamline Your Hiring, and Build
           Your Dream Team.
-        </p>
-        <div className="flex justify-center gap-4 lg:justify-normal">
+        </motion.p>
+        <motion.div
+          className="flex justify-center gap-4 lg:justify-normal"
+          variants={fadeInAnimationVariants}
+        >
           <Link href={"/"}>
             <Button variant="default">Try it out!</Button>
           </Link>
           <Link href={"/"}>
             <Button variant="outline">See more</Button>
           </Link>
-        </div>
-      </div>
-      <div>
+        </motion.div>
+      </motion.div>
+      <motion.div
+        variants={fadeInAnimationVariants}
+        initial="initial"
+        animate="animate"
+      >
         <Image
           src={"/hero.svg"}
           priority={true}
@@ -42,7 +61,7 @@ export default function Hero() {
           width={448}
           height={300}
         />
-      </div>
+      </motion.div>
     </div>
   );
 }
