1a5f5a0b52869a318cce3900327cb491859013c5
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-11-09 10:00:20 +0100

    add effect on hover and current path to nav links + prepare files for pricing and contact page

--- src/app/(landing-page)/_components/Nav.tsx
+++ src/app/(landing-page)/_components/Nav.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import Button from "~/app/ui/Button";
 import Logo from "~/app/ui/Logo";
@@ -10,13 +13,21 @@ const links: { name: string; href: string }[] = [
 ];
 
 export default function Nav() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex items-center gap-8 sm:mx-4 sm:my-2 md:mx-24 md:my-4">
       <div className="grow">
         <Logo />
       </div>
       {links.map(({ name, href }, i) => (
-        <Link href={href} key={i} className="text-lg">
+        <Link
+          href={href}
+          key={i}
+          className={`hover:text-main-600 text-lg font-normal ${
+            pathname === href && "text-main-600"
+          }`}
+        >
           {name}
         </Link>
       ))}
---
+++ src/app/(landing-page)/pricing/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Pricing() {
+  return <main>Pricing</main>;
+}
